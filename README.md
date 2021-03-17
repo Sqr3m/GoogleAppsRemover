9106c346334ab22902620c34e5584c601de9f13d
Gregory <gregory1markovich@gmail.com>
2021-03-17 15:21:40 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,23 @@
+# Google Apps Remover
+
+## Instruction
+
+- Install `git`, `adb`
+
+- ```git clone https://github.com/Sqr3m/GoogleAppsRemover```
+
+- Go to settings, find **"About phone"**, click 7 times on **Build number**, then find **"Developer options"** and go there, find the **DEBUGGING** section and finally enable **USB Debugging**
+
+- Connect your phone via cable. Wait. **Allow USB debugging *(OK)***
+
+- ```sh remover.sh```
+
+---
+
+## Troubleshoot
+
+### *If dialog doesn\'t appear try to*
+
+- *Reconnect phone*
+- *Disable/enable **USB Debugging***
+- *Reboot phone*
---
+++ remover.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+adbs="adb shell pm uninstall -k --user 0"
+
+adb devices
+
+echo "It won't delete Google Play"
+
+echo "Google"
+$adbs com.google.android.googlequicksearchbox
+
+echo "Chrome"
+$adbs com.android.chrome
+
+echo "Google Maps"
+$adbs com.google.android.apps.maps
+
+echo "Play Movies"
+$adbs com.google.android.videos
+
+echo "Play Music"
+$adbs com.google.android.music
+
+echo "Magazine"
+$adbs com.google.android.apps.magazines
+
+echo "Keep"
+$adbs com.google.android.keep
+
+echo "YouTube"
+$adbs com.google.android.youtube
+
+echo "GMail"
+$adbs com.google.android.gm
+
+echo "Google Drive"
+$adbs $adbs com.google.android.apps.docs
+
+echo "GMail Go"
+$adbs com.google.android.gm.lite
+
+echo "Google Duo"
+$adbs com.google.android.apps.tachyon
+
+echo "Google Messages"
+$adbs com.google.android.apps.messaging
+
+echo "Google Books"
+$adbs com.google.android.apps.books
